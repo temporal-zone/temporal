c7faf84344ba2c14e7ac7f0a63ff17e37275350e
george-aj <35270943+george-aj@users.noreply.github.com>
2023-08-04 15:01:58 -0500

    add testing helpers

---
+++ app/apptesting/test_helpers.go
@@ -0,0 +1,33 @@
+package apptesting
+
+import (
+	tmtypes "github.com/cometbft/cometbft/proto/tendermint/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/temporal-zone/temporal/app"
+)
+
+var (
+	ChainID = "temporal-test"
+)
+
+type SuitelessAppTestHelper struct {
+	App *app.App
+	Ctx sdk.Context
+}
+
+// Instantiates an TestHelper without the test suite
+// This is for testing scenarios where we simply need the setup function to run,
+// and need access to the TestHelper attributes and keepers (e.g. genesis tests)
+func SetupSuitelessTestHelper() SuitelessAppTestHelper {
+	s := SuitelessAppTestHelper{}
+	s.App = app.InitTestApp(true)
+	s.Ctx = s.App.BaseApp.NewContext(false, tmtypes.Header{Height: 1, ChainID: ChainID})
+	return s
+}
+
+func GetRandomAddress() sdk.AccAddress {
+	pk := ed25519.GenPrivKey().PubKey()
+
+	return sdk.AccAddress(pk.Address())
+}
