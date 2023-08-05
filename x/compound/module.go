9e5c42d0195fdb6d679c6e06f9aca5e944c80c9a
george-aj <35270943+george-aj@users.noreply.github.com>
2023-08-05 14:50:14 -0500

    add compound runner to end block

--- x/compound/module.go
+++ x/compound/module.go
@@ -140,7 +140,12 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 func (AppModule) ConsensusVersion() uint64 { return 1 }
 
 // BeginBlock contains the logic that is automatically triggered at the beginning of each block
-func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
+func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
+	err := am.keeper.RunCompounding(ctx)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // EndBlock contains the logic that is automatically triggered at the end of each block
 func (am AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
